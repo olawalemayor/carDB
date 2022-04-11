c7f8035dfcf20442b81e05d9bc6da4d0acdfec93
Olawale Mayor <olawalemayor90@gmail.com>
2022-04-11 15:23:46 +0100

    simplified header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccd-header',
   templateUrl: 'header.component.html',
-  styleUrls: ['header.component.css'],
+  styles: [
+    `
+      .brand-image {
+        width: 150px;
+      }
+    `,
+  ],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   WebLogo = {
     siteImage: 'assets/images/site-logo.png',
     logoTitle: 'chassis.ng logo',
   };
-
-  ngOnInit() {
-    const topBar = document.getElementById('top-bar') as HTMLDivElement;
-    document.addEventListener('scroll', () => {
-      topBar.classList.add('scroll-bg');
-      if (window.scrollY === 0) topBar.classList.remove('scroll-bg');
-    });
-  }
 }
